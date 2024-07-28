5b313c78a0724285ec00c1a97b0e69e343a64b92
rldinkar <144485802+rldinkar@users.noreply.github.com>
2024-07-28 22:50:55 +0530

    Update getCoinPrices.js

--- src/functions/getCoinPrices.js
+++ src/functions/getCoinPrices.js
@@ -1,21 +1,31 @@
 import axios from "axios";
-export const getCoinPrices = (id, days,priceType) => {
-    const options = {
-        method: 'GET',
-        headers: {
-            accept: 'application/json',
-            'x-cg-demo-api-key': 'CG-m2bYJMfCXFJcoEvuSBiyTEY8'
-        }
-    };
-    const prices = axios
-        .get(
-            `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=${days}`,
-            options
-        )
-        .then((response) => {
-            return response.data[priceType];
-        })
-        .catch((err) => console.error(err));
-    return prices;
 
-}
+export const getCoinPrices = async (id, days, priceType) => {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      'x-cg-demo-api-key': 'CG-m2bYJMfCXFJcoEvuSBiyTEY8'
+    }
+  };
+
+  try {
+    const response = await axios.get(
+      `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=${days}&interval=daily`,
+      options
+    );
+    if (response.data) {
+      console.log("Prices>>>", response.data);
+      if (priceType === "market_caps") {
+        return response.data.market_caps;
+      } else if (priceType === "total_volumes") {
+        return response.data.total_volumes;
+      } else {
+        return response.data.prices;
+      }
+    }
+  } catch (e) {
+    console.error('Error fetching coin prices:', e.message);
+    return [];
+  }
+};
