d4034ca4de8d295c256904d8ea2031ee8ef2ea1d
rldinkar <144485802+rldinkar@users.noreply.github.com>
2024-07-28 22:51:36 +0530

    Update index.js

--- src/components/Coin/PriceToggle.js/index.js
+++ src/components/Coin/PriceToggle.js/index.js
@@ -1,39 +1,43 @@
-import React from 'react';
-import { useState } from 'react';
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import './styles.css'
-export default function PriceToggle({priceType , handlePriceTypeChange}) {
- 
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+
+
+export default function PriceToggle({ priceType, handlePriceTypeChange }) {
   return (
-    <div className='toggle-prices'>
-    <ToggleButtonGroup
-      value={priceType}
-      exclusive
-      onChange={handlePriceTypeChange}
-      sx={{
-        "&.Mui-selected": {
-          color: "var(--blue) !important",
-        },
-        borderColor: "var(--blue)",
-        border: "unset !important",
-        "& .MuiToggleButtonGroup-grouped": {
-          border: "1px solid var(--blue)!important",
-          borderColor: "unset",
-          color: "var(--blue) !important ",
-        },
-        "& .MuiToggleButton-standard": {
-          color: "var(--blue) !important",
-        },
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginBottom: "1.5rem",
       }}
     >
-      <ToggleButton value="prices"  className='toggle-btn'>Price
-      </ToggleButton>
-      <ToggleButton value="market_cap"  className='toggle-btn'>Market Cap
-      </ToggleButton>
-      <ToggleButton value="total_volumes" className='toggle-btn'>Total Volume
-      </ToggleButton>
-    </ToggleButtonGroup>
+      <ToggleButtonGroup
+        value={priceType}
+        exclusive
+        onChange={(e) => {
+          handlePriceTypeChange(e);
+        }}
+        sx={{
+          "&.Mui-selected": {
+            color: "var(--blue) !important",
+          },
+          borderColor: "var(--blue)",
+          border: "unset !important",
+          "& .MuiToggleButtonGroup-grouped": {
+            border: "1px solid var(--blue)!important",
+            borderColor: "unset",
+            color: "var(--blue) !important ",
+          },
+          "& .MuiToggleButton-standard": {
+            color: "var(--blue) !important",
+          },
+        }}
+      >
+        <ToggleButton value="prices">Prices</ToggleButton>
+        <ToggleButton value="market_caps">Market Cap</ToggleButton>
+        <ToggleButton value="total_volumes">Total Volume</ToggleButton>
+      </ToggleButtonGroup>
     </div>
   );
 }
